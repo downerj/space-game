1de4ee969e9aee6d23fbca1fdb6d23a2d95ecf33
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-02-28 19:18:14 -0700

    Graphics stub files

---
+++ src/graphics.hxx
@@ -0,0 +1,10 @@
+#ifndef GRAPHICS_HXX
+#define GRAPHICS_HXX
+
+namespace my {
+
+class Graphics {};
+
+} // namespace my
+
+#endif // GRAPHICS_HXX
--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,16 @@
 #include <iostream>
 
 #include "game.hxx"
+#include "graphics.hxx"
 #include "window.hxx"
 
 auto main(int, char**) -> int {
   my::Window window{}; (void)window;
   std::cout << "Window initialized!\n";
+  my::Graphics graphics{}; (void)graphics;
+  std::cout << "Graphics initialized!\n";
   my::Game game{}; (void)game;
   std::cout << "Game initialized!\n";
+  
+  std::cout << "Goodbye.\n";
 }
