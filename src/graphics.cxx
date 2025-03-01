cfd7a7fb1807550c90749fa4d2ceb3d950bb3f2c
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-02-28 23:23:34 -0700

    GL debug extension

--- src/graphics.cxx
+++ src/graphics.cxx
@@ -9,6 +9,17 @@
 #include "debug.hxx"
 #include "window.hxx"
 
+namespace {
+
+#ifdef DEBUG
+auto debugMessageCallbackGL(
+  GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+  const GLchar* message, const void* userParam
+) -> void;
+#endif // DEBUG
+
+} // namespace
+
 my::Graphics::Graphics(SDL_Window* window)
 : _context{SDL_GL_CreateContext(window)} {
   if (!_context) {
@@ -27,6 +38,18 @@ my::Graphics::Graphics(SDL_Window* window)
   if (SDL_GL_SetSwapInterval(1) < 0) {
     LOG_ERROR("Warning: Unable to set VSync: " << SDL_GetError() << '\n');
   }
+#ifdef DEBUG
+  if (GLAD_GL_ARB_debug_output) {
+    LOG("GL_ARB_debug_output extension available\n");
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
+    glDebugMessageCallbackARB(
+      debugMessageCallbackGL,
+      nullptr // userParam
+    );
+  } else {
+    LOG("GL_ARB_debug_output extension unavailable\n");
+  }
+#endif // DEBUG
 }
 
 my::Graphics::~Graphics() {
@@ -41,3 +64,21 @@ auto my::Graphics::render() const -> void {
   glClearColor(0., .5, 1., 1.);
   glClear(GL_COLOR_BUFFER_BIT);
 }
+
+namespace {
+
+#ifdef DEBUG
+auto debugMessageCallbackGL(
+  GLenum, GLenum, GLuint, GLenum severity, GLsizei, const GLchar* message,
+  const void*
+) -> void {
+  switch (severity) {
+    case GL_DEBUG_SEVERITY_MEDIUM_ARB:
+    case GL_DEBUG_SEVERITY_HIGH_ARB:
+      LOG_ERROR("OpenGL error: " << message);
+      break;
+  }
+}
+#endif // DEBUG
+
+} // namespace
